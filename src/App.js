3a17f0fd831df37876bbcab48a6db9185bce449b
vignesh <vigneshdas4444@gmail.com>
2022-08-18 22:04:30 +0530

    added dynamic input box

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import AddRemoveMultipleInputFields from "./AddRemoveMultipleInputFields";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      <AddRemoveMultipleInputFields />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
